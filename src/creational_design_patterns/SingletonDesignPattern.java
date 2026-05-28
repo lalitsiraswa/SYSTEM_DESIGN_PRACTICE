d6fa9141802ea82cbea7db20fff0042b3b340ed7
Lavik <lavik@Laviks-MacBook-Pro.local>
2026-05-28 21:55:53 +0530

    Double - Checked Locking (volatile keyword)

--- src/creational_design_patterns/SingletonDesignPattern.java
+++ src/creational_design_patterns/SingletonDesignPattern.java
@@ -28,15 +28,47 @@ package creational_design_patterns;
 // Thread Un-Safe
 // synchronized keyword - A mechanism that controls the access of multiple threads to shared resources.
 // It ensures that only one thread can access a "critical section" of code at a time, preventing data corruption and race conditions.
+//class JudgeAnalytics{
+//    private static JudgeAnalytics judgeAnalytics;
+//    private int run = 0;
+//    private int submit = 0;
+//    private JudgeAnalytics(){}
+//    // Critical Section
+//    public static synchronized JudgeAnalytics getJudgeAnalyticsInstance(){
+//        if(judgeAnalytics == null){
+//            judgeAnalytics = new JudgeAnalytics();
+//        }
+//        return judgeAnalytics;
+//    }
+//    public void countRun(){
+//        run++;
+//    }
+//    public void countSubmit(){
+//        submit++;
+//    }
+//    public int getRunCount(){
+//        return run;
+//    }
+//    public int getSubmitCount(){
+//        return submit;
+//    }
+//}
+
+// Double - Checked Locking
+// It uses the volatile keyword to ensure that changes to the instance variable are immediately visible to other threads.
 class JudgeAnalytics{
-    private static JudgeAnalytics judgeAnalytics;
+    private static volatile JudgeAnalytics judgeAnalytics;
     private int run = 0;
     private int submit = 0;
     private JudgeAnalytics(){}
     // Critical Section
-    public static synchronized JudgeAnalytics getJudgeAnalyticsInstance(){
+    public static JudgeAnalytics getJudgeAnalyticsInstance(){
         if(judgeAnalytics == null){
-            judgeAnalytics = new JudgeAnalytics();
+            synchronized (JudgeAnalytics.class) {
+                if(judgeAnalytics == null) {
+                    judgeAnalytics = new JudgeAnalytics();
+                }
+            }
         }
         return judgeAnalytics;
     }
