c6392bee02cd7663eb895540213501a5175323ca
Lavik <lavik@Laviks-MacBook-Pro.local>
2026-05-29 17:12:56 +0530

    Factory Design Patter

--- src/creational_design_patterns/FactoryDesignPattern.java
+++ src/creational_design_patterns/FactoryDesignPattern.java
@@ -18,22 +18,55 @@ class Air implements Logistics{
     }
 }
 
+class Train implements Logistics{
+    @Override
+    public void transport() {
+        System.out.println("Transporting by train logic!");
+    }
+}
+
 // Currently the LogisticsService class is violating the Single Responsibility Principle.
 // It should be only responsible for transporting the Item, but currently it is also dealing with object creation.
 // This causes:
 // > tight coupling
 // > repeated code
 // >difficult maintenance
+//class LogisticsService{
+//    public void transport(String mode){
+//        if(mode.equals("Air")){
+//            Logistics logistics = new Air();
+//            logistics.transport();
+//        }
+//        else if(mode.equals("Road")){
+//            Logistics logistics = new Road();
+//            logistics.transport();
+//        }
+//    }
+//}
+
+// Following SOLID Principles
+// Open Close Principle
+class LogisticsFactory{
+    public static Logistics getLogistics(String mode){
+        if(mode.equals("Road")){
+            return new Road();
+        }
+        else if(mode.equals("Air")){
+            return new Air();
+        }
+        else if(mode.equals("Train")){
+            return new Train();
+        }
+        return null;
+    }
+}
+
+// Following SOLID Principles
+// Single Responsibility Principle
 class LogisticsService{
     public void transport(String mode){
-        if(mode.equals("Air")){
-            Logistics logistics = new Air();
-            logistics.transport();
-        }
-        else if(mode.equals("Road")){
-            Logistics logistics = new Road();
-            logistics.transport();
-        }
+        Logistics logistics = LogisticsFactory.getLogistics(mode);
+        logistics.transport();
     }
 }
 
@@ -42,5 +75,6 @@ public class FactoryDesignPattern {
         LogisticsService logisticsService = new LogisticsService();
         logisticsService.transport("Air");
         logisticsService.transport("Road");
+        logisticsService.transport("Train");
     }
 }
