ee1f71cabb9dec1c7079eed7a75d95d59213abed
Lavik <lavik@Laviks-MacBook-Pro.local>
2026-06-06 17:34:54 +0530

    Decorator Design Pattern - Problem

---
+++ src/structural_design_pattern/DecoratorDesignPattern.java
@@ -0,0 +1,16 @@
+package structural_design_pattern;
+
+// Problem
+class PlainPizza{}
+class CheesePizza extends PlainPizza{}
+class OlivePizza extends PlainPizza{}
+class StuffedPizza extends PlainPizza{}
+class CheeseStuffedPizza extends PlainPizza{}
+class CheeseOlivePizza extends PlainPizza{}
+class CheeseOliveStuffedPizza{}
+
+public class DecoratorDesignPattern {
+    static void main() {
+
+    }
+}
