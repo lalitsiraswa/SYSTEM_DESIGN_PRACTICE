b3ff53e16b6a05acfab9ef7b72c211f2c074ab36
Lavik <lavik@Laviks-MacBook-Pro.local>
2026-06-11 20:45:22 +0530

    Composite Design Pattern Chat GPT - PROBLEM

--- src/structural_design_pattern/CompositeDesignPatternChatGPT.java
+++ src/structural_design_pattern/CompositeDesignPatternChatGPT.java
@@ -47,6 +47,40 @@ class Folder implements FileSystemComponent {
     }
 }
 
+class FileClone{
+    private String name;
+    public FileClone(String name){
+        this.name = name;
+    }
+    public void show(){
+        System.out.println("File: " + name);
+    }
+}
+
+class FolderClone{
+    private String name;
+    private List<FileClone> files = new ArrayList<>();
+    private List<FolderClone> folders = new ArrayList<>();
+    public FolderClone(String name){
+        this.name = name;
+    }
+    public void addFile(FileClone file){
+        files.add(file);
+    }
+    public void addFolder(FolderClone folder){
+        folders.add(folder);
+    }
+    public void show(){
+        System.out.println("Folder: " + name);
+        for(FileClone file : files){
+            file.show();
+        }
+        for(FolderClone folder : folders){
+            folder.show();
+        }
+    }
+}
+
 //Root
 //│
 //├── file1.txt
@@ -58,6 +92,15 @@ class Folder implements FileSystemComponent {
 //      └── notes.txt
 
 public class CompositeDesignPatternChatGPT {
+    // Problem - instanceof
+    public static void display(Object object){
+        if(object instanceof FileClone){
+            ((FileClone)object).show();
+        }
+        else if(object instanceof FolderClone){
+            ((FolderClone)object).show();
+        }
+    }
     static void main() {
         Folder root = new Folder("Root");
         File file1 = new File("File-1");
@@ -74,5 +117,20 @@ public class CompositeDesignPatternChatGPT {
         root.add(documents);
 
         root.show();
+
+        System.out.println("\n");
+
+        // Problem CLIENT CODE
+        FileClone resume_file = new FileClone("resume.pdf");
+        FileClone notes_file = new FileClone("notes.txt");
+        FolderClone documents_folder = new FolderClone("Documents");
+        documents_folder.addFile(resume_file);
+        documents_folder.addFile(notes_file);
+        FolderClone root_folder = new FolderClone("Root");
+        root_folder.addFolder(documents_folder);
+        root_folder.show();
+        System.out.println("\n");
+        display(resume_file);
+        display(root_folder);
     }
 }
