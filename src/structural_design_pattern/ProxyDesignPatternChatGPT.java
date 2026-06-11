76b2582040da9689d34a7a697d3c124acabf4d1c
Lavik <lavik@Laviks-MacBook-Pro.local>
2026-06-11 21:54:26 +0530

    Proxy Design Pattern ChatGPT - SOLUTION

--- src/structural_design_pattern/ProxyDesignPatternChatGPT.java
+++ src/structural_design_pattern/ProxyDesignPatternChatGPT.java
@@ -14,10 +14,53 @@ class RealImage{
     }
 }
 
+// SOLUTION
+interface Image{
+    void display();
+}
+
+class RealImageClone implements Image{
+    private final String fileName;
+    public RealImageClone(String fileName){
+        this.fileName = fileName;
+        loadImageFromDisk();
+    }
+    private void loadImageFromDisk(){
+        System.out.println("Loading image from disk: " + fileName);
+    }
+    @Override
+    public void display() {
+        System.out.println("Displaying image: " + fileName);
+    }
+}
+
+class ProxyImage implements Image{
+    private final String fileName;
+    private RealImageClone realImageClone;
+    public ProxyImage(String fileName){
+        this.fileName = fileName;
+    }
+    @Override
+    public void display() {
+        if(realImageClone == null){
+            realImageClone = new RealImageClone(fileName);
+        }
+        realImageClone.display();
+    }
+}
+
 public class ProxyDesignPatternChatGPT {
     static void main() {
         // PROBLEM - Even if we never display it.
         // This wastes resources by loading - calling "loadImageFromDisk"
         RealImage image = new RealImage("photo.jpg");
+
+        System.out.println("\n");
+
+        // SOLUTION
+        Image image1 = new ProxyImage("image.jpg");
+        System.out.println("Image object created");
+        // Now it only load the resource when need - It only loading the image when we want to display.
+        image1.display();
     }
 }
