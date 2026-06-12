bff213abe1769806c1d6b645d9f439a4b42489cc
Lavik <lavik@Laviks-MacBook-Pro.local>
2026-06-12 17:31:11 +0530

    Bridge Design Pattern - SOLUTION

--- src/structural_design_pattern/BridgeDesignPattern.java
+++ src/structural_design_pattern/BridgeDesignPattern.java
@@ -47,5 +47,93 @@ class SmartTVUltra4KPlayer implements PlayQuality{
         System.out.println("Smart TV: Playing " + title + " in ultra 4k");
     }
 }
+
+// SOLUTION
+interface VideoQuality{
+    void load(String title);
+}
+
+class SDQuality implements VideoQuality{
+    @Override
+    public void load(String title) {
+        System.out.println("Streaming " + title + " in SD Quality");
+    }
+}
+
+class HDQuality implements VideoQuality{
+    @Override
+    public void load(String title) {
+        System.out.println("Streaming " + title + " in HD Quality");
+    }
+}
+
+class UltraHDQuality implements VideoQuality{
+    @Override
+    public void load(String title) {
+        System.out.println("Streaming " + title + " in 4K Ultra HD Quality");
+    }
+}
+
+// Image 8k Introduced
+class K8Player implements VideoQuality{
+    @Override
+    public void load(String title) {
+        System.out.println("Streaming " + title + " in 8k Quality");
+    }
+}
+
+abstract class VideoPlayer{
+    protected VideoQuality videoQuality;
+    public VideoPlayer(VideoQuality videoQuality){
+        this.videoQuality = videoQuality;
+    }
+    public abstract void play(String title);
+}
+
+class MobilePlayer extends VideoPlayer{
+    public MobilePlayer(VideoQuality videoQuality) {
+        super(videoQuality);
+    }
+
+    @Override
+    public void play(String title) {
+        System.out.println("Mobile Platform");
+        videoQuality.load(title);
+    }
+}
+
+class WebPlayer extends VideoPlayer{
+    public WebPlayer(VideoQuality videoQuality) {
+        super(videoQuality);
+    }
+    @Override
+    public void play(String title) {
+        System.out.println("Web Platform");
+        videoQuality.load(title);
+    }
+}
+
+class SmartTVPlayer extends VideoPlayer{
+    public SmartTVPlayer(VideoQuality videoQuality) {
+        super(videoQuality);
+    }
+
+    @Override
+    public void play(String title) {
+        System.out.println("Smart TV Platform");
+        videoQuality.load(title);
+    }
+}
+// Now both Streaming platform (Web Player, Mobile Player, Smart TV) and Quality (HD, 4k, 8k) are decoupled.
 public class BridgeDesignPattern {
+    static void main() {
+        VideoPlayer webPlayer = new WebPlayer(new HDQuality());
+        webPlayer.play("Take You Forward");
+        System.out.println();
+        VideoPlayer smartTvPlayer = new SmartTVPlayer(new UltraHDQuality());
+        smartTvPlayer.play("APEX");
+        System.out.println();
+        VideoPlayer smartTvPlayer8K = new SmartTVPlayer(new K8Player());
+        smartTvPlayer8K.play("ABCD");
+    }
 }
