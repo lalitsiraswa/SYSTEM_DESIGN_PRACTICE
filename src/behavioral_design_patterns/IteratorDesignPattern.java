58ffe6b39c58b5f321ef21a41891125d1ec097c6
Lavik <lavik@Laviks-MacBook-Pro.local>
2026-06-14 12:21:23 +0530

    Iterator Design Pattern - SOLUTION

--- src/behavioral_design_patterns/IteratorDesignPattern.java
+++ src/behavioral_design_patterns/IteratorDesignPattern.java
@@ -23,6 +23,76 @@ class YouTubePlayList{
     }
 }
 
+// SOLUTION
+interface PlaylistIterator{
+    boolean hasNext();
+    Video next();
+}
+
+// Concrete iterator - traversal algorithm - 1
+class YouTubePlaylistIterator implements PlaylistIterator{
+    private List<Video> videos;
+    private int position;
+
+    public YouTubePlaylistIterator(List<Video> videos){
+        this.videos = videos;
+        this.position = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position < videos.size();
+    }
+
+    @Override
+    public Video next() {
+        return hasNext() ? videos.get(position++) : null;
+    }
+}
+
+class YouTubePlaylistCopyrightIterator implements PlaylistIterator{
+    private List<Video> videos;
+    private int position;
+
+    public YouTubePlaylistCopyrightIterator(List<Video> videos){
+        this.videos = videos;
+        this.position = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position < videos.size();
+    }
+
+    @Override
+    public Video next() {
+        // TODO: Logic for Copyright and Skip those videos.
+        return hasNext() ? videos.get(position++) : null;
+    }
+}
+
+// Iterable interface
+interface Playlist{
+    PlaylistIterator createIterator();
+    PlaylistIterator createCopyrightIterator();
+}
+
+class YouTubePlayListClone implements Playlist{
+    private List<Video> videos = new ArrayList<>();
+    public void addVideo(Video video){
+        videos.add(video);
+    }
+    @Override
+    public PlaylistIterator createIterator() {
+        return new YouTubePlaylistIterator(videos);
+    }
+
+    @Override
+    public PlaylistIterator createCopyrightIterator() {
+        return new YouTubePlaylistCopyrightIterator(videos);
+    }
+}
+
 public class IteratorDesignPattern {
     static void main() {
         // PROBLEM - Video is exposed on the client side.
@@ -37,6 +107,18 @@ public class IteratorDesignPattern {
         // what data structure the class is using to store the data.
         for(Video video : playList.getVideos()){
             System.out.println(video.getTitle());
+        }
+
+        // SOLUTION Client - Code
+        System.out.println();
+        // Here we hide the all the implementation details from the client like traversal logic and what type of data structure
+        // it is using for storing data (list, set, graph etc.).
+        YouTubePlayListClone youTubePlayListClone = new YouTubePlayListClone();
+        youTubePlayListClone.addVideo(new Video("System Design was HARD until I Learned these 30 Concepts"));
+        youTubePlayListClone.addVideo(new Video("8 Most Important System Design Concepts You Should Know"));
+        PlaylistIterator playlistIterator = youTubePlayListClone.createIterator();
+        while(playlistIterator.hasNext()){
+            System.out.println(playlistIterator.next().getTitle());
         }
     }
 }
