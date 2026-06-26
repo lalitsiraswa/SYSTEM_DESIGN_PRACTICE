7e249e22d385d43114d6bebabc550cf8f4c9856f
Lavik <lavik@Laviks-MacBook-Pro.local>
2026-06-26 22:34:25 +0530

    Template Method Design Pattern CHATGPT - SOLUTION

--- src/behavioral_design_patterns/TemplateMethodDesignPatternCHATGPT.java
+++ src/behavioral_design_patterns/TemplateMethodDesignPatternCHATGPT.java
@@ -48,8 +48,45 @@ class Coffee {
 // addSugar()
 // appears in every class.
 
+// SOLUTION
+abstract class Beverage {
+    // Template Method
+    public final void prepare() {
+        boilWater();
+        addMainIngredient();
+        pourIntoCup();
+        addSugar();
+    }
+    private void boilWater() {
+        System.out.println("Boiling Water");
+    }
+    private void pourIntoCup() {
+        System.out.println("Pouring into Cup");
+    }
+    private void addSugar() {
+        System.out.println("Adding Sugar");
+    }
+    protected abstract void addMainIngredient();
+}
+
+class TeaClone extends Beverage {
+    @Override
+    protected void addMainIngredient() {
+        System.out.println("Adding Tea Leaves");
+    }
+}
+class CoffeeClone extends Beverage {
+    @Override
+    protected void addMainIngredient() {
+        System.out.println("Adding Coffee Powder");
+    }
+}
 public class TemplateMethodDesignPatternCHATGPT {
     static void main() {
-
+        Beverage tea = new TeaClone();
+        tea.prepare();
+        System.out.println();
+        Beverage coffee = new CoffeeClone();
+        coffee.prepare();
     }
 }
