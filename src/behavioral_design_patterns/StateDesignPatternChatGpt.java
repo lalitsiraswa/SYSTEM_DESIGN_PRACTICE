c2246fc39acb8d430644943e817bb08774a915d9
Lavik <lavik@Laviks-MacBook-Pro.local>
2026-06-30 22:05:31 +0530

    State Design Pattern ChatGpt - SOLUTION

--- src/behavioral_design_patterns/StateDesignPatternChatGpt.java
+++ src/behavioral_design_patterns/StateDesignPatternChatGpt.java
@@ -6,7 +6,7 @@ package behavioral_design_patterns;
 // 3. Hard to Maintain
 // We have all the business ligic under same class like: STOP, PLAY, PAUSE.
 // Let's say tomorrow if new state introduces we have to change out existing class that will break Open-Close Principle
-class MediaPlayer {
+class MediaPlayerContextMediaPlayer {
     private String state = "STOPPED";
     public void pressPlay() {
         if(state.equals("STOPPED"))
@@ -42,8 +42,86 @@ class MediaPlayer {
     }
 }
 
+// SOLUTION
+
+class MediaPlayerContext {
+    private PlayerState state = new StoppedState();
+    public void setState(PlayerState state) {
+        this.state = state;
+    }
+    public void play() {
+        state.play(this);
+    }
+    public void pause() {
+        state.pause(this);
+    }
+    public void stop() {
+        state.stop(this);
+    }
+}
+
+interface PlayerState {
+    void play(MediaPlayerContext player);
+    void pause(MediaPlayerContext player);
+    void stop(MediaPlayerContext player);
+}
+
+class StoppedState implements PlayerState {
+    @Override
+    public void play(MediaPlayerContext player) {
+        System.out.println("Starting Playback");
+        player.setState(new PlayingState());
+    }
+    @Override
+    public void pause(MediaPlayerContext player) {
+        System.out.println("Cannot Pause");
+    }
+    @Override
+    public void stop(MediaPlayerContext player) {
+        System.out.println("Already Stopped");
+    }
+}
+
+class PlayingState implements PlayerState {
+    @Override
+    public void play(MediaPlayerContext player) {
+        System.out.println("Already Playing");
+    }
+    @Override
+    public void pause(MediaPlayerContext player) {
+        System.out.println("Paused");
+        player.setState(new PausedState());
+    }
+    @Override
+    public void stop(MediaPlayerContext player) {
+        System.out.println("Stopped");
+        player.setState(new StoppedState());
+    }
+}
+
+class PausedState implements PlayerState {
+    @Override
+    public void play(MediaPlayerContext player) {
+        System.out.println("Resuming Playback");
+        player.setState(new PlayingState());
+    }
+    @Override
+    public void pause(MediaPlayerContext player) {
+        System.out.println("Already Paused");
+    }
+    @Override
+    public void stop(MediaPlayerContext player) {
+        System.out.println("Stopped");
+        player.setState(new StoppedState());
+    }
+}
+
 public class StateDesignPatternChatGpt {
     static void main() {
-
+        MediaPlayerContext player = new MediaPlayerContext();
+        player.play();
+        player.pause();
+        player.play();
+        player.stop();
     }
 }
