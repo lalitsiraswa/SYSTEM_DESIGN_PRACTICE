39665ce2c07771c666680b9cdb487679f3ebef11
Lavik <lavik@Laviks-MacBook-Pro.local>
2026-07-04 17:07:45 +0530

    Mediator Design Pattern - SOLUTION

--- src/behavioral_design_patterns/MediatorDesignPattern.java
+++ src/behavioral_design_patterns/MediatorDesignPattern.java
@@ -29,8 +29,57 @@ class User{
     }
 }
 
+// SOLUTION
+interface DocumentSessionMediator{
+    void broadcastChange(String change, UserClone sender);
+    void join(UserClone user);
+}
+
+class CollaborativeDocument implements DocumentSessionMediator{
+    private final List<UserClone> users = new ArrayList<>();
+    @Override
+    public void broadcastChange(String change, UserClone sender) {
+        for(UserClone user : users){
+            if(user != sender){
+                user.receiveChange(change, sender);
+            }
+        }
+    }
+    @Override
+    public void join(UserClone user) {
+        users.add(user);
+    }
+}
+
+class UserClone{
+    protected String name;
+    protected DocumentSessionMediator mediator;
+    public UserClone(String name, DocumentSessionMediator mediator){
+        this.name = name;
+        this.mediator = mediator;
+    }
+    public void makeChange(String change){
+        System.out.println(name + " edited the document: " + change);
+        mediator.broadcastChange(change, this);
+    }
+    public void receiveChange(String change, UserClone send){
+        System.out.println(name + " saw change from " + send.name + ": \"" + change +"\"");
+    }
+}
+
 public class MediatorDesignPattern {
     static void main() {
+        CollaborativeDocument document = new CollaborativeDocument();
 
+        UserClone alice = new UserClone("Alice", document);
+        UserClone bob = new UserClone("Bob", document);
+        UserClone charlie = new UserClone("Charlie", document);
+
+        document.join(alice);
+        document.join(bob);
+        document.join(charlie);
+
+        alice.makeChange("Added project title");
+        bob.makeChange("Corrected grammar in paragraph - 2");
     }
 }
