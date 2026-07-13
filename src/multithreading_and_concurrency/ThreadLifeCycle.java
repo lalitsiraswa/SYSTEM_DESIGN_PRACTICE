6a8028c2b4fd2ecd9afdf791e813294d32c8bd14
Lavik <lavik@Laviks-MacBook-Pro.local>
2026-07-13 22:25:33 +0530

    Thread Life Cycle

---
+++ src/multithreading_and_concurrency/ThreadLifeCycle.java
@@ -0,0 +1,32 @@
+package multithreading_and_concurrency;
+
+public class ThreadLifeCycle {
+    static void main() throws InterruptedException {
+        Thread worker = new Thread(() -> {
+            System.out.println("Worker started.");
+            try{
+                System.out.println("Worker is sleeping for 3 seconds...");
+                Thread.sleep(3000);
+            } catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            System.out.println("Worker finished.");
+        });
+
+        // 1. NEW
+        System.out.println("1. State: " + worker.getState());
+        // Start thread
+        worker.start();
+        // 2. RUNNABLE
+        System.out.println("2. State: " + worker.getState());
+        // Give the worker time to enter sleep()
+        Thread.sleep(500);
+        System.out.println("3. State: " + Thread.currentThread().getState());
+        // 3. TIMED_WAITING
+        System.out.println("3. State: " + worker.getState());
+        // wait for worker
+        worker.join();
+        // 4. TERMINATED
+        System.out.println("4. State: " + worker.getState());
+    }
+}
