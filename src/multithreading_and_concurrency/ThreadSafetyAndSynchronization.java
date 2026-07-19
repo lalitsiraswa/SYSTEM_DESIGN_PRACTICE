2d5ae36f142c9c5adb67d9a3b04d378fb1270b76
Lavik <lavik@Laviks-MacBook-Pro.local>
2026-07-19 16:02:55 +0530

    Atomic Variables

--- src/multithreading_and_concurrency/ThreadSafetyAndSynchronization.java
+++ src/multithreading_and_concurrency/ThreadSafetyAndSynchronization.java
@@ -2,6 +2,12 @@ package multithreading_and_concurrency;
 
 // Race Condition
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
+
 // Purchase counter with no protection
 class PurchaseCounter{
     // Shared count value
@@ -158,12 +164,85 @@ class VolatileExample{
     }
 }
 
+// Atomic Variables:-
+// Java provides a set of classes under the java.util.concurrent.atomic package, designed to handle common types like integers
+// and booleans in a thread-safe, high-performance way - without using locks.
+// Real-Life Analogy: Social Media Likes
+// Think of a "Like" counter on a popular Instagram post. Millions of users can tap the Like button at the same time.
+// The system needs to safely update the like count without missing any, and it must do this without slowing everyone down with locks.
+// Atomic variables work the same way - offering safe, fast updates even under heavy traffic.
+// Common Atomic Classes
+// AtomicInteger: for atomic operations on integers
+// AtomicBoolean: for managing flags safely
+// (There are also AtomicLong, AtomicReference, etc., for more advanced use cases.)
+// How Atomic Variables Work
+// All atomic classes use a technique called Compare-And-Swap (CAS) at the hardware level. This is what makes them lock-free
+// and highly performant.
+// CAS Concept:-
+// CAS stands for Compare-And-Swap. It is a low-level CPU/hardware instruction that checks if a memory location holds an expected value,
+// and if so, swaps it with a new value - all in one atomic step. Here's how it works in simple terms:
+// “If the current value is what I expect it to be, update it with a new value. Otherwise, try again.”
+// This way, threads don't block each other-they simply keep retrying until they succeed, avoiding race conditions without using locks.
+
+class PurchaseAtomicCounter{
+    // A thread-safe integer backed by hardware-level CAS(Compare-And-Swap)
+    private final AtomicInteger likes = new AtomicInteger();
+    // Atomically add 1 to the like counter
+    public void incrementLikes(){
+        int prev, next;
+        do{
+            // Step 1  – read the current value.
+            // (Maybe outdated if another thread wins the race.)
+            prev = likes.get();
+            // Step 2  – compute the desired next value.
+            next = prev + 1;
+            // Step 3  – attempt to swap:
+            /*          “If current value(likes variable value) is still ‘prev’, set it to ‘next’.”
+             *          Returns true on success, false if another thread
+             *          already changed the value (retry needed).
+             */
+        } while (!likes.compareAndSet(prev, next));
+    }
+    // Read-only accessor
+    public int getCount() {
+        return likes.get();
+    }
+    public void purchaseAtomicCounterMain() throws ExecutionException, InterruptedException {
+        System.out.println(Thread.currentThread().getName());
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Runnable task = () ->{
+            System.out.println(Thread.currentThread().getName());
+            for(int i = 1; i <= 1000; i++){
+                incrementLikes();
+            }
+        };
+        Future<?> f1 = executorService.submit(task);
+        Future<?> f2 = executorService.submit(task);
+        Future<?> f3 = executorService.submit(task);
+        Future<?> f4 = executorService.submit(task);
+        Future<?> f5 = executorService.submit(task);
+        // get() waits until the task finishes.
+        // comment out all the .get() method and check result.
+        f1.get();
+        f2.get();
+        f3.get();
+        f4.get();
+        f5.get();
+        executorService.shutdown();
+        System.out.println(getCount());
+    }
+}
+
 public class ThreadSafetyAndSynchronization {
-    static void main() throws InterruptedException {
+    static void main() throws InterruptedException, ExecutionException {
+        System.out.println(Thread.currentThread().getName());
 //        RaceConditionDemo.raceConditionDemoMain();
 //        FixRaceConditionDemo.fixRaceConditionDemoMain();
 
-        VolatileExample volatileExample = new VolatileExample();
-        volatileExample.volatileExampleMain();
+//        VolatileExample volatileExample = new VolatileExample();
+//        volatileExample.volatileExampleMain();
+
+        PurchaseAtomicCounter purchaseAtomicCounter = new PurchaseAtomicCounter();
+        purchaseAtomicCounter.purchaseAtomicCounterMain();
     }
 }
